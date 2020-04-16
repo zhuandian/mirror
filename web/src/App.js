c96db1a96b5a7361904c5897d28baf403b368d43
xiedong11 <xiedong11@aliyun.com>
2020-04-16 21:39:12 +0800

    添加投影监听

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,61 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            code: 0,
+            contentEntity: {}
+        }
+    }
+
+    componentDidMount() {
+
+        let that = this
+        setInterval(function () {
+            let query = window.bmob.Query("SendEntity");
+            query.find().then((sendEntity) => {
+
+                console.log(that.state.code+"------------"+sendEntity[0].code)
+                if (that.state.code < sendEntity[0].code) {
+
+                    window.bmob.Query("ContentEntity").get(sendEntity[0].contentEntity.objectId)
+                        .then((res) => {
+                            that.setState({
+                                contentEntity: res
+                            })
+                            console.log(res);
+                        })
+                }
+                that.setState({
+                    code: sendEntity[0].code,
+
+                })
+
+            })
+        }, 2000)
+    }
+
+    render() {
+        let {contentEntity} = this.state
+
+        return (
+            <div className="App">
+
+                {contentEntity.type == 1 ? <img src={contentEntity.content}/> : ""}
+
+                {contentEntity.type == 2 ? <span>{contentEntity.content}</span> : ""}
+
+                {contentEntity.type == 3 ? <video src={contentEntity.content} controls="autoplay" width="100%" height="100%"/> : ""}
+
+            </div>
+        );
+    }
+
+
 }
 
 export default App;
---
+++ web/src/initBmob.js
@@ -0,0 +1,8 @@
+import bmob from "hydrogen-js-sdk";
+
+window.initBmob = function () {
+    bmob.initialize('425913fbe114b8b3d57cb9f36aec4c17', '04db5d28c483054a4be4eb8248db7d1b');
+}
+
+window.initBmob()
+window.bmob = bmob
